f200f9b41e3d42487c831fb3ef519686dd381536
Tom3127 <tomas.reznicek312@gmail.com>
2023-04-20 18:38:52 +0200

    pohybování do stran, země, postavička

--- main.py
+++ main.py
@@ -1,50 +1,55 @@
 import pygame
-import os
 
 pygame.init()
 
-screen_width = 500
-screen_height = 1000
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Happy Jump")
-window_icon = pygame.image.load("assets/window_icon.png")
-pygame.display.set_icon(window_icon)
+if __name__ == "__main__":
 
-bg_clr = (2, 152, 219)
+    screen_width = 500
+    screen_height = 1000
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Happy Jump")
+    window_icon = pygame.image.load("assets/window_icon.png")
+    pygame.display.set_icon(window_icon)
 
-fps = 60
+    bg_clr = (2, 152, 219)
 
-# images
-platform_img = pygame.image.load("assets/platform.png").convert_alpha()
+    # images
+    platform = pygame.image.load("assets/platform.png").convert_alpha()
+    start_surface = pygame.image.load("assets/start_surface.png").convert_alpha()
+    character = pygame.image.load("assets/character.png").convert_alpha()
 
-# platforms
+    #character
+    char_width = character.get_width()
+    char_pos_x = 215
+    char_pos_y = 880
 
-
-def draw_window():
-    screen.fill(bg_clr)
-    pygame.display.flip()
-    pygame.display.update()
-
-def platform_generate():
-    screen.blit(platform_img, (200, 500))
-
-#main loop
-def main():
+    #main loop function
     clock = pygame.time.Clock()
-    run = True
+    program = True
+    game = True # POZDĚJI UPRAVIT NA SWITCH (po přidání před-herního menu s tlačítky atd.)
 
-    while run:
-        clock.tick(fps)
+    while program:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    platform_generate()
+                program = False
+                pygame.quit()
+        # character moving
+        # ARROW inputs (moving on x axis)
+        keys = pygame.key.get_pressed()
 
-        draw_window()
+        if char_pos_x > 0:
+            if keys[pygame.K_LEFT]:
+                    char_pos_x -= 8
+        if char_pos_x < screen_width - char_width:
+            if keys[pygame.K_RIGHT]:
+                    char_pos_x += 8
 
-    pygame.quit()
+        screen.fill(bg_clr)
+        screen.blit(start_surface, (0, 950))
+        screen.blit(character, (char_pos_x, char_pos_y))
+        screen.blit(platform, (100, 150))
 
-if __name__ == "__main__":
-    main()
+        pygame.display.flip()
+        pygame.display.update()
+        clock.tick(120) #frames per second
+
--- platform_generation.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import random
-
-platform_count = 10
-
-for i in range(platform_count):
-    random.choice()
