8fae966e8cff76f375ce35a1a6bfc5b1b887b693
Tom3127 <tomas.reznicek312@gmail.com>
2023-04-22 20:18:39 +0200

    added jumping and platform gen sketch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -16,12 +17,37 @@ if __name__ == "__main__":
     # images
     platform = pygame.image.load("assets/platform.png").convert_alpha()
     start_surface = pygame.image.load("assets/start_surface.png").convert_alpha()
-    character = pygame.image.load("assets/character.png").convert_alpha()
+    character_img = pygame.image.load("assets/character.png").convert_alpha() # default image for the character
+    character_left = pygame.image.load("assets/character_left.png") # character moving left image
+    character_right = pygame.image.load("assets/character_right.png") # character moving right image
 
     #character
-    char_width = character.get_width()
+    char_width = character_img.get_width()
+    char_height = character_img.get_height()
     char_pos_x = 215
     char_pos_y = 880
+    char_bottom = char_pos_y + char_height
+    character = character_img
+
+    y_gravity = 1
+    jump_height = 30
+    y_velocity = jump_height
+    jumping = False
+
+    #platform generation
+    platform_count = 10
+    for i in range(platform_count):
+        platforms_x = [0, 100, 200, 300, 400, 500]
+        platforms_y = []
+        platforms = []
+        platform_x = random.choice(platforms_x)
+        platform_y = random.randint(0, 1000)
+        
+        
+
+    #start surface
+    start_surface_x = 0
+    start_surface_y = 950
 
     #main loop function
     clock = pygame.time.Clock()
@@ -33,23 +59,41 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 program = False
                 pygame.quit()
-        # character moving
-        # ARROW inputs (moving on x axis)
-        keys = pygame.key.get_pressed()
 
-        if char_pos_x > 0:
-            if keys[pygame.K_LEFT]:
-                    char_pos_x -= 8
-        if char_pos_x < screen_width - char_width:
-            if keys[pygame.K_RIGHT]:
-                    char_pos_x += 8
+        # character moving
+        keys = pygame.key.get_pressed() # getting dictionary of key inputs
+
+        if keys[pygame.K_LEFT] and char_pos_x > 0: 
+            char_pos_x -= 8
+            character = character_left
+        
+
+        if keys[pygame.K_RIGHT] and char_pos_x < screen_width - char_width:
+            char_pos_x += 8
+            character = character_right
+
+        if keys[pygame.K_UP]:
+            jumping = True
+        
+        for current_platform_y in platforms_y:
+            if char_bottom == current_platform_y:
+                jumping = True
+        
+        if jumping:     # adding gravity to the character
+            char_pos_y -= y_velocity
+            y_velocity -= y_gravity
+            if y_velocity < -jump_height:
+                jumping = False
+                y_velocity = jump_height
 
         screen.fill(bg_clr)
-        screen.blit(start_surface, (0, 950))
+        screen.blit(start_surface, (start_surface_x, start_surface_y))
+        screen.blit(platform, (platform_x, platform_y))
         screen.blit(character, (char_pos_x, char_pos_y))
-        screen.blit(platform, (100, 150))
+
+        character = character_img # setting back the default image for character
 
         pygame.display.flip()
         pygame.display.update()
-        clock.tick(120) #frames per second
+        clock.tick(60) #frames per second
 
