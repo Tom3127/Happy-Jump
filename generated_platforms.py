3036baa96b35891ad08c2c6a3ae654bbd99c24b2
Tom3127 <tomas.reznicek312@gmail.com>
2023-04-26 16:29:51 +0200

    small changes, new file for platforms

---
+++ generated_platforms.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Platform:
+    def __init__(self, x, y, image):
+        pass
+
+    def generation():
+        pass
+
--- main.py
+++ main.py
@@ -11,24 +11,23 @@ if __name__ == "__main__":
     pygame.display.set_caption("Happy Jump")
     window_icon = pygame.image.load("assets/window_icon.png")
     pygame.display.set_icon(window_icon)
-
+    
     bg_clr = (2, 152, 219)
 
     # images
-    platform_img = pygame.image.load("assets/platform.png").convert_alpha()
-    start_surface = pygame.image.load("assets/start_surface.png").convert_alpha()
-    character_img = pygame.image.load("assets/character.png").convert_alpha() # default image for the character
-    character_left = pygame.image.load("assets/character_left.png").convert_alpha() # character moving left image
-    character_right = pygame.image.load("assets/character_right.png").convert_alpha() # character moving right image
-    character_debug = pygame.image.load("assets/debug_character.png").convert_alpha()
+    platform_img = pygame.image.load("assets/platform.png").convert_alpha() # image of the platforms
+    start_surface = pygame.image.load("assets/start_surface.png").convert_alpha() # image of the ground
+    character_img = pygame.image.load("assets/player.png").convert_alpha() # default image for the player
+    character_left = pygame.image.load("assets/player_left.png").convert_alpha() # player moving left image
+    character_right = pygame.image.load("assets/player_right.png").convert_alpha() # player moving right image
+    character_debug = pygame.image.load("assets/player_debug.png").convert_alpha() # image of debug player
 
-    #character
+    #player
     char_width = character_img.get_width()
     char_height = character_img.get_height()
     char_pos_x = 215
     char_pos_y = 880
-    char_bottom = char_pos_y + char_height
-    character = character_img
+    player = character_img
 
     y_gravity = 1
     jump_height = 30
@@ -61,23 +60,23 @@ if __name__ == "__main__":
                 program = False
                 pygame.quit()
 
-        # character moving
+        # player moving
         keys = pygame.key.get_pressed() # getting key inputs
 
         if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and char_pos_x > 0: 
             char_pos_x -= 8
-            character = character_left
+            player = character_left
         
 
         if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and char_pos_x < screen_width - char_width:
             char_pos_x += 8
-            character = character_right
+            player = character_right
 
         if keys[pygame.K_UP] or keys[pygame.K_SPACE] or keys[pygame.K_w]:
             jumping = True
         
         
-        if jumping: # adding jumping to the character
+        if jumping: # adding jumping to the player
             char_pos_y -= y_velocity
             y_velocity -= y_gravity
             if y_velocity < -jump_height:
@@ -92,15 +91,15 @@ if __name__ == "__main__":
             platform_rect = pygame.Rect(dict_in_list['x'], dict_in_list['y'], 100, 25)
             screen.blit(platform_img, platform_rect)
             
-        # drawing character
+        # drawing player
         character_rect = pygame.Rect(char_pos_x, char_pos_y, 70, 70)
-        screen.blit(character, character_rect)
+        screen.blit(player, character_rect)
 
         #collision
         if character_rect.colliderect(platform_rect):
             print("KOLIZE!!!!!!")
 
-        character = character_img # setting back the default image for character
+        player = character_img # setting back the default image for player
 
         pygame.display.flip()
         pygame.display.update()
